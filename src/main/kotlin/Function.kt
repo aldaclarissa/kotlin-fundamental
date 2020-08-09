1286b83d4edad91df400706f696c596e132cbf18
aldaclarissa <aldacsn.wnj@gmail.com>
2020-08-09 11:16:12 +0700

    test

---
+++ src/main/kotlin/Function.kt
@@ -0,0 +1,5 @@
+fun main(){
+    val user = setUser("Alda", 16)
+    println(user)
+}
+fun setUser (name:String, age: Int) = "My name is $name, I like reading wattpad, and I'm $age years old"
---
+++ src/main/kotlin/HelloKotlin.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val name = "Alda Clarissa Syahda Nur"
+    val hobby = "Baca Wattpad"
+
+    print("Nama saya ")
+    println(name)
+    print("Hobi saya ")
+    println(hobby)
+}
